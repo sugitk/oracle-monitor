1b68d313a607351207ba109973fbd1d82e91c6f6
SUGIMURA Takashi <takashi.sugimura@ntt.com>
2016-08-02 13:23:39 +0900

    add about dba_temp_files

--- 13_datafiles.sql
+++ 13_datafiles.sql
@@ -1,9 +1,13 @@
 set pages 1000
-col tablespace_name format a15
-col file_name format a45
+set lines 100
+col tablespace_name format a20
+col file_name format a50
 col bytes format 999999999999
 
 select tablespace_name, bytes, file_name from dba_data_files
+order by tablespace_name, file_name;
+
+select tablespace_name, bytes, file_name from dba_temp_files
 order by tablespace_name, file_name;
 
 quit
